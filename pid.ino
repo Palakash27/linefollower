81e89e99ee37babaf76df9f8fd60d9b4e5781df7
iamnaman7 <33201647+iamnaman7@users.noreply.github.com>
2017-11-16 17:02:30 +0530

    Add files via upload

--- pid.ino
+++ pid.ino
@@ -1,24 +1,17 @@
-#include <SoftwareSerial.h>
-SoftwareSerial bt(A0,A1);
-
-
-
 int s1,s2,s3,s4,s5;
-int pos;
-int error;
-int kp;
+int pos=0;
+
+int kp,ki,kd;
 int powerdif;
-int der;
+int der=0;
 int enl=5;
 int enr=6;
 int l1=10;
 int l2=11;
 int r1=12;
 int r2=13;
-int x;
 
-int lasterror=0;
-int integral=0;
+
 
 void setup() {
 
@@ -31,32 +24,47 @@ void setup() {
   pinMode(enl,OUTPUT);
   pinMode(enr,OUTPUT);
   Serial.begin(9600);
-bt.begin(9600);
+
 }
 
 void loop(){
-  if(bt.available())
-  {
-    x=bt.read();
-    kp+=x;
-  }
+
+kp=analogRead(A0)/16;      
  
-kp=1;
+ki=analogRead(A1)/16;
+
+kd=analogRead(A2)/1600000;
 
   pid();
-  
-
+ Serial.print(kp);
+ Serial.print("\t"); 
+Serial.print(ki);
+ Serial.print("\t");
+ Serial.println(kd);
+ 
 }
 void pid()
-{
+{while(1)
+{s1=digitalRead(8);
+s2=digitalRead(7);
+s3=digitalRead(A5);
+s4=digitalRead(3);
+s5=digitalRead(2);
+int lasterror=0;
+int integral=0;
+int error;
   readline();
   error=0-pos;
+ /*Serial.print("E = ");
+ Serial.print(error);*/
   der=error-lasterror;
   integral=integral+error;
   lasterror= error;
-  powerdif=kp*error;
+  powerdif=(kp*error)+(ki*integral)+(kd*der);
   if(powerdif>255)
   powerdif=255;
+Serial.print("\tdef = ");
+Serial.println(powerdif);
 
 if(powerdif<-255)
  powerdif=-255;
@@ -83,7 +91,11 @@ if(powerdif<-255)
  digitalWrite(r2,LOW);
  
 }
-else
+if(s1==0&&s2==0&&s3==0&&s4==0&&s5==1)
+return;
+if(s1==1&&s2==0&&s3==0&&s4==0&&s5==0)
+ return;
+/*else
 {
  analogWrite(enl,255);
  analogWrite(enr,255);
@@ -92,37 +104,65 @@ else
  digitalWrite(l2,LOW);
  digitalWrite(r2,LOW);
  
+}*/
+  
 }
-  
- 
-  
+ if(s1==0&&s2==0&&s3==0&&s4==0&&s5==1)
+ {
+  analogWrite(enl,255);
+ analogWrite(enr,255);
+   digitalWrite(l1,HIGH);
+  digitalWrite(r1,LOW);
+  digitalWrite(l2,LOW);
+  digitalWrite(r2,HIGH);
+ }
+  if(s1==1&&s2==0&&s3==0&&s4==0&&s5==0)
+ {
+  analogWrite(enl,255);
+ analogWrite(enr,255);
+   digitalWrite(l1,LOW);
+  digitalWrite(r1,HIGH);
+  digitalWrite(l2,HIGH);
+  digitalWrite(r2,LOW);
+ }
 }
 void readline()
 {
-s1=digitalRead(8);
-s2=digitalRead(7);
-s3=digitalRead(A5);
-s4=digitalRead(3);
-s5=digitalRead(2);
- if(s1==1&&s2==0&&A5==0&&s4==0&&s5==0)
- pos=4;
- if(s1==1&&s2==1&&A5==0&&s4==0&&s5==0)
- pos=3;
- if(s1==0&&s2==1&&A5==0&&s4==0&&s5==0)
- pos=2;
- if(s1==0&&s2==1&&A5==1&&s4==0&&s5==0)
- pos=1;
- if(s1==0&&s2==0&&A5==1&&s4==0&&s5==0)
- pos=0;
 
- if(s1==0&&s2==0&&A5==1&&s4==1&&s5==0)
+/*Serial.print("S1 = ");
+Serial.print(s1);
+Serial.print("\tS2 = ");
+Serial.print(s2);
+Serial.print("\tS3 = ");
+Serial.print(s3);
+Serial.print("\tS4 = ");
+Serial.print(s4);
+Serial.print("\tS5 = ");
+Serial.print(s5);
+Serial.print("\t");
+delay(10);*/
+
+
+
+if(s1==1&&s2==1&&s3==0&&s4==0&&s5==0)
+ pos=3;
+else if(s1==0&&s2==1&&s3==0&&s4==0&&s5==0)
+ pos=2;
+else if(s1==0&&s2==1&&s3==1&&s4==0&&s5==0)
+ pos=1;
+else if(s1==0&&s2==0&&s3==1&&s4==0&&s5==0)
+pos=0;
+
+else if(s1==0&&s2==0&&s3==1&&s4==1&&s5==0)
  pos=-1;
- if(s1==0&&s2==0&&A5==0&&s4==1&&s5==0)
+else if(s1==0&&s2==0&&s3==0&&s4==1&&s5==0)
  pos=-2;
- if(s1==0&&s2==0&&A5==0&&s4==1&&s5==1)
+else if(s1==0&&s2==0&&s3==0&&s4==1&&s5==1)
  pos=-3;
- if(s1==0&&s2==0&&A5==0&&s4==0&&s5==1)
- pos=-4;
+
+//Serial.print("\t POS = ");
+//Serial.println(pos);
+//delay(100);
 
  
 }
